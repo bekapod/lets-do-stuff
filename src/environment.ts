8ed96a9a5e2e0067876ba847bb3ca3a4f9a45ef9
Becky <bckyjns@gmail.com>
2017-11-01 17:34:38 +0000

    add env file

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const firebaseProps = {
+  apiKey: 'AIzaSyAoMkLzQkFC4h66q2VGlKBde4ollNP8F6o',
+  authDomain: 'lets-do-stuff-app.firebaseapp.com',
+  databaseURL: 'https://lets-do-stuff-app.firebaseio.com',
+  projectId: 'lets-do-stuff-app',
+  storageBucket: 'lets-do-stuff-app.appspot.com',
+  messagingSenderId: '481111358887'
+};
